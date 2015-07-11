2b63a9e721a88ccb0e78d900b6b50fbe81a99d82
Prayoch Rujira <jay@odd-e.co.th>
2015-07-11 21:05:24 +0700

    add sample html

---
+++ ts/test/RandomNumberWordGeneratorSpec.ts
@@ -0,0 +1,7 @@
+describe('RandomNumberWordGenerator', () => {
+    it('should random provided answer', () => {
+        var generator = new RandomNumberWordGenerator();
+
+        var answers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+        })
+    })
