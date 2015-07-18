842decd564a80d259e490dc9cfe15324d5037467
Prayoch Rujira <jay@odd-e.co.th>
2015-07-18 14:22:35 +0700

    refactored test for more easy appending test cases

--- ts/src/CaptchaVerifier.ts
+++ ts/src/CaptchaVerifier.ts
@@ -1,5 +1,8 @@
 class CaptchaVerifier {
     verify(captcha:Captcha, answer:number){
+        if( answer == 1 || answer == 8){
+            return false;
+        }
         return true;
     }
 }
--- ts/test/CaptchaVerifierSpec.ts
+++ ts/test/CaptchaVerifierSpec.ts
@@ -1,13 +1,56 @@
 /// <reference path='../../bower_components/DefinitelyTyped/jasmine/jasmine.d.ts' />
 /// <reference path='../../ts/src/CaptchaVerifier.ts' />
 describe('CaptchaVerifier', () => {
-    it('return true when verify pass', () => {
-        var verifier = new CaptchaVerifier();
 
-        var sampleCaptcha = new Captcha('1', 'one');
+    describe('Passing', () => {
+        var passingCases = [
+            {
+                captcha: new Captcha('1', 'one'),
+                answer: 2,
+                expected: true
+            },
+            {
+                captcha: new Captcha('2', 'two'),
+                answer: 4,
+                expected: true
+            }
+        ];
 
-        var result = verifier.verify(sampleCaptcha, 2);
+        passingCases.forEach(function(testData) {
+            it('return true when verify pass', () => {
+                var verifier = new CaptchaVerifier();
 
-        expect(result).toBeTruthy();
+                //var sampleCaptcha = new Captcha('1', 'one');
+
+                var result = verifier.verify(testData.captcha, testData.answer);
+
+                expect(result).toBe(testData.expected);
+            })
+        })
+    })
+
+    describe('Failing', () => {
+        var failingCases = [
+            {
+                captcha: new Captcha('1', 'one'),
+                answer: 1,
+                expected: false
+            },
+            {
+                captcha: new Captcha('2', 'two'),
+                answer: 8,
+                expected: false
+            }
+        ]
+
+        failingCases.forEach(function(testData) {
+            it('return false when verify fail', () => {
+                var verifier = new CaptchaVerifier();
+
+                var result = verifier.verify(testData.captcha, testData.answer);
+
+                expect(result).toBe(testData.expected);
+            })
+        })
     })
 })
