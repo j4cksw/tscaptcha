fc4d557e9c8a671463b547d6db4f3d0bd1bffd04
Prayoch Rujira <jay@odd-e.co.th>
2015-07-20 23:47:08 +0700

    remove comment

--- ts/test/RandomNumberWordGeneratorSpec.ts
+++ ts/test/RandomNumberWordGeneratorSpec.ts
@@ -6,9 +6,9 @@ describe('RandomNumberWordGenerator', () => {
         var fakeRandomizer = jasmine.createSpyObj('FakeRandomizer', ['next'])
         fakeRandomizer.next.and.returnValue(1)
         generator.setRandomizer(fakeRandomizer)
-        //var answers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+
         var result = generator.generate();
-        //expect(answers.indexOf(generator.generate())).toBeGreaterThan(-1);
+
         expect(result.getDisplay()).toBe('one')
         expect(result.getValue()).toBe(1)
     })
